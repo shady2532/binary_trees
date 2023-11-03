dfa34a66a1090a3f26176f0bf07faaa7ca6279d6
shady2532 <shadyrizk2532@gmail.com>
2023-11-03 04:19:49 +0200

    last, i guess

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	node->n = value;
 	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
 	return (node);
 }
